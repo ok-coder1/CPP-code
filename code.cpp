49c6135006ef61c82f785295609a10788addfb50
ok-coder1 <106904247+ok-coder1@users.noreply.github.com>
2022-11-23 19:53:26 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string name;
+  cout << "What's your name";
+  cin >> name;
+  cout << "Hello, " << name << endl;
+}
